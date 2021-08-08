f4c5adafecd679bded4ca19878d3d1bbf3eaff95
wxx123 <stellarwxx@163.com>
2021-08-08 17:43:15 +0800

    add spring security demo

---
+++ spring-security/src/main/java/com/demo/ssdemo/SsdemoApplication.java
@@ -0,0 +1,17 @@
+package com.demo.ssdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 登录名称:admin teacher1 student1
+ * 密码:123456(所有都是)
+ */
+@SpringBootApplication
+public class SsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsdemoApplication.class, args);
+    }
+
+}
